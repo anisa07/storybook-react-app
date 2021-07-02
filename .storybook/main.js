6ef839a7f68baeaa748aa1d20295d44855431a0b
anisa07 <askany07@gmail.com>
2021-07-02 02:07:33 +0200

    add push yaml file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.tsx'],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-a11y",
-    "storybook-addon-material-ui"
-  ]
+    stories: ['../src/components/**/*.stories.tsx'],
+    refs: {
+        'design-system': {
+            title: 'My design system',
+            //👇 The url provided by Chromatic when it was deployed
+            url: 'https://www.chromatic.com/manage?appId=60de331f3e7bce003b21bfa4&branch=main',
+        },
+    },
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-a11y",
+        "storybook-addon-material-ui"
+    ]
 }
